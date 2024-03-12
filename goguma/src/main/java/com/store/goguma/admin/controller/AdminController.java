2c20f2abbc33d15ea23990355b2081263600dc06
pdill <dbtmzp1@naver.com>
2024-03-13 04:01:48 +0900

    admin 뷰페이지 작업

--- goguma/src/main/java/com/store/goguma/admin/controller/AdminController.java
+++ goguma/src/main/java/com/store/goguma/admin/controller/AdminController.java
@@ -1,9 +1,14 @@
 package com.store.goguma.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.store.goguma.user.dto.OauthDTO;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -11,14 +16,31 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	private HttpSession httpSession;
 	
 	// 관리자 유저 수정 페이지
 	// localhost://admin/modiUser
+	
 	@GetMapping("/user")
+	public String User(Model model){
+		
+		OauthDTO user = (OauthDTO) httpSession.getAttribute("principal");
+		
+		model.addAttribute("user",user);
+	
+		return "admin/admin_user";
+	}
+	
+	
+	
+	@GetMapping("/userModi")
 	public String modifyUser(){
 		
+	
 		
-		return "";
+	
+		return "admin/admin_user_modi";
 	}
 	
 	@GetMapping("/Emoji")
@@ -32,7 +54,7 @@ public class AdminController {
 	public String salesHistory() {
 		
 		
-		return "";
+		return "admin/admin_payment_history";
 	}
 	
 	@GetMapping("/notice")
--- goguma/src/main/webapp/WEB-INF/view/header.jsp
+++ goguma/src/main/webapp/WEB-INF/view/header.jsp
@@ -70,7 +70,7 @@
 <script
 	src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body class="seo_version">
+<body class="seo_version" style="background: #fff">
 
 	<header class="header header_style_01">
 		<!-- nav 부분 navbar 가 안먹음 -->
