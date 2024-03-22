7717673a32eee2a1cf004950c22e8cad121b9df3
StarsinLiver <141594965+StarsinLiver@users.noreply.github.com>
2024-03-23 02:05:25 +0900

    Revert "merge kang -> dev"

--- goguma/src/main/java/com/store/goguma/category/controller/CategoryPageController.java
+++ goguma/src/main/java/com/store/goguma/category/controller/CategoryPageController.java
@@ -17,14 +17,4 @@ public class CategoryPageController {
 	public String cateSetting() {
 		return "board/admin-cate-setting";
 	}
-	
-	@GetMapping("/cate-setting2")
-	public String cateSetting2() {
-		return "board/admin-cate-setting2";
-	}
-	
-	@GetMapping("/cate-modify")
-	public String cateSetting3() {
-		return "board/freeBoard_modify";
-	}
 }
